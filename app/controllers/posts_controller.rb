3b40c7339edc9edb2b8886adc41671acf7c230ea
sgc88 <sehribancheng@yahoo.com>
2017-11-14 18:28:29 -0800

    added Edit button, can edit and save posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,18 @@ class PostsController < ApplicationController
       render "new"
     end
   end
+
+  def edit
+      @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
   private def post_params
     params.require(:post).permit(:title, :body)
   end
